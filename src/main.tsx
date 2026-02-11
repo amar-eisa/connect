e39c239b477e5ea14094b951539a6470811cb38b
Amar Eisa <38670741+amar-eisa@users.noreply.github.com>
2026-02-11 19:28:02 +0000

    save

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "@fontsource/tajawal/400.css";
-import "@fontsource/tajawal/500.css";
-import "@fontsource/tajawal/700.css";
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
--- supabase/migrations/20260205120035_cb852e29-9e97-4a95-9355-f8cf2da16904.sql
+++
@@ -1,5 +0,0 @@
--- Add DELETE policy to restrict deletion to admins only
-CREATE POLICY "Only admins can delete contact requests" 
-ON public.contact_requests 
-FOR DELETE 
-USING (has_role(auth.uid(), 'admin'::app_role));
